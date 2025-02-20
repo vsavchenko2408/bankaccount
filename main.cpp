fda30f5447d1725a594906889e5a2b4720254ab9
DerMeister <v.savchenko2408@gmail.com>
2025-02-20 23:00:43 +0200

    do main program

--- main.cpp
+++ main.cpp
@@ -2,9 +2,67 @@
 #include <deque>
 #include "account.h"
 
-size_t hello()
+short hello();
+Account &find_account();
+int main()
 {
-    size_t choise = 0;
+    short choise;
+    std::deque<Account> all_accounts;
+    do
+    {
+        choise = hello();
+        /*
+        if (choise == 1)
+        {
+            all_accounts.emplace_back(Account());
+        }
+        if (choise == 2)
+        {
+            find_account(); // need realize
+        }
+        if (choise == 3)
+        {
+            std::cout << all_accounts.size() << std::endl; // or maybe show last ID
+        }
+            */
+        switch (choise)
+        {
+        case 1:
+        {
+#ifdef WIN32
+            system("cls");
+#endif
+            all_accounts.emplace_back(Account());
+            break;
+        }
+        case 2:
+        {
+#ifdef WIN32
+            system("cls");
+#endif
+            find_account(); // need realize
+            break;
+        }
+        case 3:
+        {
+#ifdef WIN32
+            system("cls");
+#endif
+            std::cout << all_accounts.size() << std::endl; // or maybe show last ID
+            break;
+        }
+        }
+    } while (choise != 0);
+
+#ifdef WIN32
+    system("pause");
+#endif
+    return 0;
+}
+
+short hello()
+{
+    short choise = 0;
     std::cout << "\t\t\tProgram Bank Account!\n\n"
               << std::endl;
     std::cout << "\tTo create account enter 1: \n";
@@ -15,42 +73,7 @@ size_t hello()
 
     return choise;
 }
-
-int main()
+Account &find_account()
 {
-    size_t choise = hello();
-    std::deque<Account> all_accounts;
-    while (choise != 0)
-    {
-        switch (choise)
-        {
-        case 1:
-        {
-            all_accounts.emplace_back(Account());
-            hello();
-            break;
-        }
-        case 2:
-        {
-            // Account find_acc();
-            hello();
-            break;
-        }
-        case 0:
-        {
-            hello();
-        }
-        default:
-        {
-            std::cerr << "Wrong choise!";
-            hello();
-            break;
-        }
-        }
-    }
-
-#ifdef WIN32
-    system("pause");
-#endif
-    return 0;
+    // need realize
 }
